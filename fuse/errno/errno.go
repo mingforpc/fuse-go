d52c6639031783f728fb883a6659c5a7e73d2405
ming <jianmingforpc@gmail.com>
2018-11-27 13:34:22 +0800

    添加错误代码

--- fuse/errno/errno.go
+++ fuse/errno/errno.go
@@ -3,9 +3,13 @@ package errno
 const SUCCESS = 0
 
 const FAILED = -1
-const ENOENT = -2  /* No such file or directory */
-const EAGAIN = -11 /* Try again */
-const EACCES = -13 /* Permission denied */
-const EEXIST = -17 /* File exists */
-const ENOTDIR = 20 /* Not a directory */
-const ENOSYS = -38 /* Invalid system call number */
+const ENOENT = -2          /* No such file or directory */
+const EAGAIN = -11         /* Try again */
+const EACCES = -13         /* Permission denied */
+const EEXIST = -17         /* File exists */
+const ENOTDIR = 20         /* Not a directory */
+const ENOSYS = -38         /* Invalid system call number */
+const ENODATA = -61        /* No data available */
+const ENOATTR = ENODATA    /* XATTR_REPLACE was specified, and the attribute does not exist. */
+const EOPNOTSUPP = -95     /* Operation not supported on transport endpoint */
+const ENOTSUP = EOPNOTSUPP /* setxattr: The namespace prefix of name is not valid. */
